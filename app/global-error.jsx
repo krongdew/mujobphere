cde0971851c881e90597cd658c4708f445d521f2
krongdew <dewwiisunny14@gmai.com>
2025-01-16 09:35:47 +0700

    pc

--- app/global-error.jsx
+++ app/global-error.jsx
@@ -1,6 +1,6 @@
 'use client';
-
-export default function Error({ error, reset }) {
+ 
+export default function GlobalError({ error, reset }) {
   return (
     <div className="error-page-wrapper">
       <div className="content">
--- next.config.js
+++ next.config.js
@@ -2,12 +2,16 @@
 const withNextIntl = require('next-intl/plugin')('./i18n.js');
 
 const nextConfig = {
+    output: 'standalone',
     images: {
       formats: ['image/webp'],
       deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
       imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     },
-    output: 'standalone',
+    i18n: {
+      locales: ['th', 'en'],
+      defaultLocale: 'th',
+    },
     images: {
       unoptimized: true,
     },
