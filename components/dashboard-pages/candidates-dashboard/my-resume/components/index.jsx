2ca6298481d46df240a358b4e5268eb2119aa570
DewDewtopTop <dewtop@DewTops-Mac-mini.local>
2025-01-18 18:49:08 +0700

    lazy

--- components/dashboard-pages/candidates-dashboard/my-resume/components/index.jsx
+++ components/dashboard-pages/candidates-dashboard/my-resume/components/index.jsx
@@ -1,33 +1,37 @@
 'use client'
 
-import AddPortfolio from "./AddPortfolio";
-import Awards from "./Awards";
-import Education from "./Education";
-import Experiences from "./Experiences";
-import SkillsMultiple from "./SkillsMultiple";
+import { Suspense, lazy } from 'react';
+
+// Lazy load components
+const Education = lazy(() => import('./Education'));
+const Experiences = lazy(() => import('./Experiences'));
+const Awards = lazy(() => import('./Awards'));
+const SkillsMultiple = lazy(() => import('./SkillsMultiple'));
 
 const Index = () => {
   return (
     <div className="default-form">
       <div className="row">
         <div className="form-group col-lg-12 col-md-12">
-          <Education />
-          <Experiences />
+          <Suspense fallback={<div>Loading Education...</div>}>
+            <Education />
+          </Suspense>
+          <Suspense fallback={<div>Loading Experiences...</div>}>
+            <Experiences />
+          </Suspense>
         </div>
 
         <div className="form-group col-lg-12 col-md-12">
-          <Awards />
+          <Suspense fallback={<div>Loading Awards...</div>}>
+            <Awards />
+          </Suspense>
         </div>
-
-    
 
         <div className="form-group col-lg-12 col-md-12">
-          <SkillsMultiple />
+          <Suspense fallback={<div>Loading Skills...</div>}>
+            <SkillsMultiple />
+          </Suspense>
         </div>
-
-        {/* <div className="form-group col-lg-12 col-md-12">
-          <AddPortfolio />
-        </div> */}
       </div>
     </div>
   );
