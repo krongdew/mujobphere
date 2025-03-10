c0f1fd02fbf04fcdb8800c4f0d46abb978488e67
DewDewtopTop <dewtop@DewTops-Mac-mini.local>
2025-03-10 20:04:16 +0700

    del

--- app/[locale]/admin-dashboard/applicant-details/layout.jsx
+++
@@ -1,9 +0,0 @@
-// app/[locale]/employers-dashboard/company-profile/layout.jsx
-export const metadata = {
-    title: "Admin Dashboard",
-    description: "Admin Dashboard - MUJobPhere",
-  };
-  
-  export default function Layout({ children }) {
-    return children;
-  }
