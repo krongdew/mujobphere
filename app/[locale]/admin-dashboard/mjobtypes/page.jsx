c2b6facf2771db77a9fd5db6d7eadf7041a48173
krongdew <dewwiisunny14@gmai.com>
2025-03-25 11:53:16 +0700

    mtype

--- app/[locale]/admin-dashboard/mjobtypes/page.jsx
+++ app/[locale]/admin-dashboard/mjobtypes/page.jsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-// app/admin-dashboard/job-types/page.js
->>>>>>> e42a34e29b2780e1a7046b32717a1fb96c33db0f
 "use client";
 
 import JobTypes from "@/components/dashboard-pages/admin-dashboard/job-types";
